9eda0007387ab3c3e21c848f275e8362c0ab2b0f
Allan Kiche <tle2819@students.mu.ac.ke>
2024-01-08 11:28:08 +0300

    notes2

--- src/notes.js
+++ src/notes.js
@@ -9,4 +9,27 @@ export const newNote = async(note, tags) => {
     }
 
     await insertDB(newNote)
+    return newNote
+}
+
+//get all notes
+export const getAllNotes = async () => {
+    const {notes} = await getDB()
+    return notes
+}
+
+// find notes
+export const findNotes = async (filter) => {
+    const {notes} = await getDB()
+    return notes.filter(note => note.content.toLowerCase().includes(filter.toLowerCase()))
+
+}
+
+export const removeNote = async id => {
+    const notes = await getDB()
+    const match = notes.find(note => note.id = id)
+
+    if (match) {
+        const newNotes = notes.filter(note => note.id != id)
+    }
 }
