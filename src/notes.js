f7268bd7b1d2fcdc0931cefada8584fbf863ad35
Allan Kiche <tle2819@students.mu.ac.ke>
2024-01-08 10:48:03 +0300

    notes2

--- src/notes.js
+++ src/notes.js
@@ -1,4 +1,12 @@
 import { insertDB, saveDB, getDB} from "./db";
 
 // create a new note
-export const newNote = async(note, tags)
+export const newNote = async(note, tags) => {
+    const newNote = {
+        tags,
+        id: Date.now(),
+        content: note
+    }
+
+    await insertDB(newNote)
+}
