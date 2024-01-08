535a79a761a422795b4af960c57e784608bb2e70
Allan Kiche <tle2819@students.mu.ac.ke>
2024-01-08 06:23:05 +0300

    fs module

--- test.js
+++ test.js
@@ -10,15 +10,22 @@
 
 // wait(3000, () => {console.log('3s')})
 
-const wait = (time) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve()
-        }, time )
-    }
-)}
+// const wait = (time) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve()
+//         }, time )
+//     }
+// )}
 
-wait(3000)
-.then(() => {
-    return wait(2000)
-})
+// wait(3000)
+// .then(() => {
+//     return wait(2000)
+// })
+
+import fs from 'node:fs'
+
+const readPjson = () => {
+    const pjsonPath = new Url('./package.json', import.meta.url).pathname
+    return JSON.parse(fs.readFileSync('./package.json'))
+}
