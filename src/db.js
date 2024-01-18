3ce105e0ad73f24e180ea21597ff16647410db89
Allan Kiche <tle2819@students.mu.ac.ke>
2024-01-18 10:54:20 +0300

    test

--- src/db.js
+++ src/db.js
@@ -1,20 +1,20 @@
 import fs from 'node:fs/promises'
 
-const DB_PATH = new URL('.db/json', import.meta.url).pathname
+const DB_PATH = new URL('../db.json', import.meta.url).pathname
 
 export const getDB = async () => {
-    const db = await fstat.readFile(DB_PATH, 'utf-8')
-    return JSON.parse(db)
+  const db = await fs.readFile(DB_PATH, 'utf-8')
+  return JSON.parse(db)
 }
 
-export const saveDB = async (db)=> {
-    await fs.writeFile(DB_PATH, JSON.stringify(db, null, 2))
-    return db
+export const saveDB = async (db) => {
+  await fs.writeFile(DB_PATH, JSON.stringify(db, null, 2))
+  return db
 }
 
-export const insertDB = async (note) => {
-    const db = await getDB()
-    db.notes.push(note)
-    await saveDB(db)
-    return note
+export const insert = async (data) => {
+  const db = await getDB()
+  db.notes.push(data)
+  await saveDB(db)
+  return data 
 }
--- src/notes.js
+++ src/notes.js
@@ -1,39 +1,35 @@
-import { insertDB, saveDB, getDB} from "./db";
+import { insert, getDB, saveDB } from './db.js'
 
-// create a new note
-export const newNote = async(note, tags) => {
-    const newNote = {
-        tags,
-        id: Date.now(),
-        content: note
-    }
-
-    await insertDB(newNote)
-    return newNote
+export const newNote = async (note, tags) => {
+  const data = {
+    tags,
+    content: note,
+    id: Date.now(),
+  }
+  await insert(data)
+  return data
 }
-
-//get all notes
 export const getAllNotes = async () => {
-    const {notes} = await getDB()
-    return notes
+  const db = await getDB()
+  return db.notes
 }
 
-// find notes
-export const findNotes = async filter => {
-    const {notes} = await getDB()
-    return notes.filter(note => note.content.toLowerCase().includes(filter.toLowerCase()))
-
+export const findNotes = async (filter) => {
+  const notes = await getAllNotes()
+  return notes.filter(note => note.content.toLowerCase().includes(filter.toLowerCase()))
 }
 
-export const removeNote = async id => {
-    const {notes} = await getDB()
-    const match = notes.find(note => note.id === id)
+export const removeNote = async (id) => {
+  const notes = await getAllNotes()
+  const match = notes.find(note => note.id === id)
 
-    if (match) {
-        const newNotes = notes.filter(note => note.id !== id)
-        await saveDB({notes: newNotes})
-        return id 
-    }
+  if (match) {
+    const newNotes = notes.filter(note => note.id !== id)
+    await saveDB({notes: newNotes})
+    return id
+  }
 }
 
-export const removeAllNotes = () => saveDB({notes: []})
+export const removeAllNotes = async () => {
+  await saveDB({notes: []})
+}
